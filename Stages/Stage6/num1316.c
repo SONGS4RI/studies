0eb058e5931356a7d44ee2684a0fa438dc7d04b1
Jahoon Lee <aoleejohn@naver.com>
2022-05-13 16:25:37 +0900

    clear but not  good codes

--- Stages/Stage6/num1316.c
+++ Stages/Stage6/num1316.c
@@ -1,7 +1,34 @@
 #include<stdio.h>
 int main()
 {
+    int n,cnt=0;
+    char arr[100] = {0,};
+    scanf("%d",&n);
+    for(int i = 0; i<n; i++)
+    {
+        int j = 0;
+        scanf("%s",&arr);
+        while(arr[j]!=0)
+        {
+            int k = j+1,con=0;
+            
+            while (arr[k]!=0)
+            {
+                if(arr[k]==arr[k-1]) con++;
+                else con=0;
+                if(arr[j]==arr[k] && con==0)
+                {
+                    cnt--;
+                    break;
+                }
+                k++;
+            }
+            if(arr[j]==arr[k] && k-j!=1) break;
+            j++;
+        }
+        cnt++;
+    }
+    printf("%d",cnt);
 
-    
     return 0;
 }
