28560c571d73acf8f886c559f045678e85abae9a
Jahoon Lee <aoleejohn@naver.com>
2022-05-19 10:26:23 +0900

    first commit

--- Stages/Stage8/num2581.c
+++ Stages/Stage8/num2581.c
@@ -1,24 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    int n,cnt=0,input;
-    scanf("%d",&n);
-    for(int i = 0; i<n; i++)
-    {
-        scanf("%d",&input);
-        int j =2;
-        if(input == 1 ) cnt--;
-        while(j<input)
-        {
-            if((input/j)*j==input && j!=input) 
-            {
-                cnt--;
-                break;
-            }
-            else j++;
-        }
-        cnt++;
-    }
-    printf("%d",cnt);
+    
     return 0;
 }
