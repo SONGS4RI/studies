5da0ab8636f168f4f1b7477e1245cb385ff6e0f3
Jahoon Lee <aoleejohn@naver.com>
2022-05-31 14:27:35 +0900

    first commit

--- Stages/Stage11/num18870.c
+++ Stages/Stage11/num18870.c
@@ -1,29 +1,50 @@
 #include<stdio.h>
-typedef struct
-{
-    char num;
-    int index;
-}res;
+#include<string.h>
 
+int Bsearch(int arr[], int target)
+{
+    int low = 0;
+    int high = strlen(arr)-1;
+    int mid;
+
+    while(low<=high)
+    {
+        mid = (low+high)/2;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] > target) high = mid-1;
+        else low = mid +1;
+    }
+    return -1;
+}
+int unique(int *arr,int size)
+{
+    int i,j=0;
+    for(int i=1; i<size;i++)
+    {
+        if(arr[j]==arr[i]) continue;
+        arr[++j] = arr[i];
+    }
+    return ++j;
+}
 int compare(const void *a,const void *b)
 {
-    res A = *(res*) a;
-    res B = *(res*) b;
-    if(A.num>B.num) return 1;
-    else if(A.num<B.num) return -1;
+    int A = *(int*) a;
+    int B = *(int*) b;
+    if(A>B) return 1;
+    else if(A<B) return -1;
     return 0;
 }
 int main()
 {
     int n;
     scanf("%d",&n);
-    res arr[n];
+    int arr[n];
+    for(int i=0; i<n;i++) arr[i] =0;
     for(int i=0;i<n;i++)
     {
-        scanf("%s ",&arr[i].num);
-        arr[i].index = i;
+        scanf(" %d",&arr[i]);
     }
-    qsort(arr,n,sizeof(res),compare);
+    qsort(arr,n,sizeof(int),compare);
     
 
     return 0;
