4c629b3aa5b0bfdaacdae9be2f6dc87ddcc349ba
Jahoon Lee <aoleejohn@naver.com>
2022-05-31 18:04:29 +0900

    correct

--- Stages/Stage11/num18870.c
+++ Stages/Stage11/num18870.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
-int Bsearch(int arr[], int target)
+int Bsearch(int arr[], int target,int cnt)
 {
     int low = 0;
-    int high = strlen(arr)-1;
+    int high = cnt;
     int mid;
 
     while(low<=high)
@@ -19,7 +20,7 @@ int Bsearch(int arr[], int target)
 int unique(int *arr,int size)
 {
     int i,j=0;
-    for(int i=1; i<size;i++)
+    for(i=1; i<size;i++)
     {
         if(arr[j]==arr[i]) continue;
         arr[++j] = arr[i];
@@ -39,13 +40,18 @@ int main()
     int n;
     scanf("%d",&n);
     int arr[n];
+    int res[n];
     for(int i=0; i<n;i++) arr[i] =0;
     for(int i=0;i<n;i++)
     {
         scanf(" %d",&arr[i]);
     }
+    for(int i=0; i<n; i++) res[i] = arr[i];
     qsort(arr,n,sizeof(int),compare);
-    
-
+    int cnt = unique(arr,n);  // n-cnt °¡ Áßº¹°¹¼ö
+    for(int i=0; i<n ;i++)
+    {
+        printf("%d ",Bsearch(arr,res[i],cnt));
+    }
     return 0;
 }
