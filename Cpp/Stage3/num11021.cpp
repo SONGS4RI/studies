cf91620fe9ac908ce66758eb741fb4ae20bc4005
Jahoon Lee <aoleejohn@naver.com>
2022-06-07 11:06:49 +0900

    first commit

---
+++ Cpp/Stage3/num11021.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,q,w;
+    cin >> n;
+    for(int i=1; i<n+1;i++)
+    {
+        cin >> q >> w;
+        cout << "Case #" << i << ": " << q+w << "\n";
+    }
+    return 0;
+}
