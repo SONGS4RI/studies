8209dc0540e684a4d950120b110ff6d3a9a2f289
Jahoon Lee <aoleejohn@naver.com>
2022-06-08 16:01:34 +0900

    first commit

--- Cpp/Stage2/num2480.cpp
+++ Cpp/Stage2/num2480.cpp
@@ -1,1 +1,21 @@
 #include<iostream>
+using namespace std;
+int main()
+{
+    int q,w,e;
+    cin >> q >> w >> e;
+    if(q == w && w == e) cout << 10000 + q*1000;
+    else if(q != w && w!=e && e!= q)
+    {
+        if (q>w && q>e) cout << q*100;
+        else if (w>q && w>e) cout << w*100;
+        else cout << e*100;
+    }
+    else
+    {
+        if(q == w) cout << 1000 + q*100;
+        else if(w==e) cout << 1000 + w*100;
+        else cout << 1000 + e*100;
+    }
+    return 0;
+}
