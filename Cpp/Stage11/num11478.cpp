9876c7e42338f3d281e398a291be27908d47e919
Jahoon Lee <aoleejohn@naver.com>
2022-06-28 16:57:45 +0900

    first commit

--- Cpp/Stage11/num11478.cpp
+++ Cpp/Stage11/num11478.cpp
@@ -1,29 +1,25 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <set>
 using namespace std;
+ 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     cout.tie(NULL);
+    cin.tie(NULL); 
     string s;
-    vector<string> v;
     cin >> s;
-    for(int i=0; i<s.length();i++)
-    {
-        for(int k=0;k<i+1;k++) // 2times
-        {
-            string temp;
-            for(int j=0;j<s.length()-i;j++) // i=1 , 
-            {
-                temp +=s[j+k];
-            }
-            v.push_back(temp);
+ 
+    set<string> set;
+ 
+    string str = "";
+    for (int i = 0; i < s.size(); i++) {
+        for (int j = i; j < s.size(); j++) {
+            str += s[j];
+            set.insert(str);
         }
+        str = "";
     }
-    sort(v.begin(),v.end());
-    v.erase(unique(v.begin(),v.end()),v.end());
-    cout << v.size();
-    return 0;
+ 
+    cout << set.size();
 }
