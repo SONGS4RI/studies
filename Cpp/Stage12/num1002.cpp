0d3a6ca9f464dc477d90b02840d00dc22a6e4f31
Jahoon Lee <aoleejohn@naver.com>
2022-07-03 03:23:07 +0900

    first commit

--- Cpp/Stage12/num1002.cpp
+++ Cpp/Stage12/num1002.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 int main()
 {
-    int t,x1,x2,y1,y2,r1,r2,temp,d;
+    int t,x1,x2,y1,y2,r1,r2,temp;
+    double d;
     cin >> t;
     for(int i=0;i<t;i++)
     {
@@ -13,7 +14,7 @@ int main()
             temp = x2;
             x2 = x1;
             x1 = temp;
-            temp = y2;
+            temp = y2;\
             y2 = y1;
             y1 = temp;
             temp = r2;
@@ -21,9 +22,9 @@ int main()
             r1 = temp;
         }
         d = sqrt(pow(abs(x1-x2),2)+pow(abs(y1-y2),2));
-        if(d>r1+r2 || d+r1<r2)cout << 0 << "\n";
+        if(d==0&&r1==r2)cout << -1 << "\n";
+        else if(d>r1+r2 || d+r1<r2) cout << 0 << "\n";
         else if(d==r1+r2 || r2==d+r1) cout << 1 << "\n";
-        else if(d==0&&r1==r2) cout << -1 << "\n";
         else cout << 2 << "\n";
     }
     return 0;
