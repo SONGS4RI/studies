9e38f55230eaf3d3b4289156b1b37b921ddf1df7
Jahoon Lee <aoleejohn@naver.com>
2022-07-12 19:32:19 +0900

    first commit

--- Cpp/Stage14/num14888.cpp
+++ Cpp/Stage14/num14888.cpp
@@ -2,61 +2,43 @@
 #include<vector>
 using namespace std;
 int n, res_max=-1000000000, res_min=1000000000, res;
-void func(vector<pair<int,bool>> v,int res,int plus,int minus,int multiple,int devide)
+void func(int num[],vector<pair<int,bool>> v_op,int cnt,int res)
 {
-    if(plus==0 && minus==0 && multiple==0 && devide==0)
+    if(cnt==n-1)
     {
         if(res>res_max) res_max =res;
         if(res<res_min) res_min =res;
+        return;
     }
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n-1;i++)
     {
-        if(!v[i].second)
+        if(!v_op[i].second)
         {
-            v[i].second = true;
-            if(multiple!=0)
-            {
-                res*=v[i].first;
-                func(v,res,plus,minus,multiple-1,devide);
-            }
-            else if(devide!=0)
-            {
-                res/=v[i].first;
-                func(v,res,plus,minus,multiple,devide-1);
-            }
-            else if(plus!=0) 
-            {
-                res+=v[i].first;
-                func(v,res,plus-1,minus,multiple,devide);
-            }
-            else if(minus!=0)
-            {
-                res-=v[i].first;
-                func(v,res,plus,minus-1,multiple,devide);
-            }
-            v[i].second = false;
+            int temp = res;
+            v_op[i].second = true;
+            if(v_op[i].first==0) res += num[cnt+1];
+            else if(v_op[i].first==1) res -= num[cnt+1];
+            else if(v_op[i].first==2) res *= num[cnt+1];
+            else if(v_op[i].first==3) res /= num[cnt+1];
+            func(num,v_op,cnt+1,res);
+            res = temp;
+            v_op[i].second = false;
         }
     }
 }
 int main()
 {
     cin >> n;
-    vector<pair<int,bool>> v;
-    int num,arr[4];
-    for(int i=0;i<n;i++)
-    { 
-        cin >> num;
-        v.push_back(pair<int,bool>(num,false));
-    }
+    int num[n],arr[4];
+    vector<pair<int,bool>> v_op;
+    for(int i=0;i<n;i++) cin >> num[i];
     for(int i=0;i<4;i++) cin >> arr[i];
-    
-    for(int i=0;i<n;i++)
+    for(int i=0;i<4;i++) 
     {
-        res = v[i].first;
-        v[i].second = true;
-        func(v,res,arr[0],arr[1],arr[2],arr[3]);
-        v[i].second = false ;
+        while(arr[i]--) v_op.push_back(pair<int,bool>(i,false));
     }
+    res = num[0];
+    func(num,v_op,0,res);
     cout << res_max << "\n" << res_min;
     return 0;
 }
