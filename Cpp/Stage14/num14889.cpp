d843fe9a64c71f9d5e49168ee24d67b46012bd9a
Jahoon Lee <aoleejohn@naver.com>
2022-07-13 21:31:35 +0900

    second commit

--- Cpp/Stage14/num14889.cpp
+++ Cpp/Stage14/num14889.cpp
@@ -1,38 +1,38 @@
 #include<iostream>
-#include<vector>
 using namespace std;
-int n,res_min=100,sel[10];
+int n,res_min=100000,sel[10];
 int arr[21][21]={0,};
 bool visited[21]={0,};
-void func(int cnt,int res,int sel[],int arr[][21])
+int func(int cnt,int sel[],int arr[][21])
 {
-    for(int i=1;i<=cnt;i++)
+    int res=0;
+    for(int i=0;i<cnt;i++)
         {
-            for(int j=1;j<=cnt;j++) // 1,2,3
+            for(int j=0;j<cnt;j++)
             {
                 if(i!=j) res+= arr[sel[i]][sel[j]];
             }
         }
+    return res;
 }
-void dfs(int cnt, bool visited[], int arr[][21],int num)
+void dfs(int cnt, bool visited[21], int arr[21][21])
 {
     if(cnt == n/2)
     {
-        int res_a=0,res_b=0,k=0;
-        func(cnt,res_a,sel,arr);
+        int k=0,sel_b[n/2],temp;
         for(int i=1;i<=n;i++)
-            if(!visited[i]) sel[k++] = i;
-        func(cnt,res_b,sel,arr);
-        if(abs(res_a-res_b)<res_min) res_min = abs(res_a-res_b);
+            if(!visited[i]) sel_b[k++] = i;
+        temp = func(cnt,sel,arr) - func(cnt,sel_b,arr);
+        if(abs(temp)<res_min) res_min = abs(temp);
     }
     
-    for(int i=num;i<=n;i++)
+    for(int i=1;i<=n;i++)
     {
         if(!visited[i])
         {
             visited[i] = true;
             sel[cnt] = i;
-            dfs(cnt+1,visited,arr,num+1);
+            dfs(cnt+1,visited,arr);
             visited[i] = false;
         }
     }
@@ -42,7 +42,7 @@ int main()
     cin >> n;
     for(int i=1;i<=n;i++)
         for(int j=1;j<=n;j++) cin >> arr[i][j];
-    dfs(0,visited,arr,1);
+    dfs(0,visited,arr);
     cout << res_min;
     return 0;
 }
