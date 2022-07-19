84be54ba2c79167b358db0b3d7798887a7efec5e
Jahoon Lee <aoleejohn@naver.com>
2022-07-19 15:56:50 +0900

    first commit

--- Cpp/Stage15/num1463.cpp
+++ Cpp/Stage15/num1463.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
     int x,dp[1000001]={0,};
-    cin >> x;
-    
-    
+    cin >> x;    
     for(int i=2;i<=x;i++)
     {
-        if(i%3==0) dp[i] = dp[i/3]+1;
-        else if(i%2==0)  dp[i] = dp[i/2]+1;
-        else dp[i] = dp[i-1]+1;
+        if(i%6==0) dp[i] = min(min(min(dp[i/3]+1,dp[i/2]+1),dp[i-1]+1),dp[i-2]+2);
+        else if(i%3==0) dp[i] = min(min(dp[i/3]+1,dp[i-1]+1),dp[i-2]+2);
+        else if(i%2==0)  dp[i] = min(min(dp[i/2]+1,dp[i-1]+1),dp[i-2]+2);
+        else dp[i] = min(dp[i-1]+1,dp[i-2]+2);
     }
+    cout << dp[x];
     return 0;
 }
