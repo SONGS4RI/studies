5ebbf3450213dcad21db0ec2f16fb365b0dcd17c
Jahoon Lee <aoleejohn@naver.com>
2022-07-19 22:31:48 +0900

    first commit

---
+++ Cpp/Stage15/num11053.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n,arr[1001],dp[1001]={0,},num_min=1001,cnt=0;
+    for(int i=0;i<n;i++)
+    {
+        cin >> arr[i];
+        if(arr[i]<num_min) num_min = arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        
+    }
+    
+
+    return 0;
+}
