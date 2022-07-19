f5f66fa0c83c6bd5b9bcda8d3e327ecbc03c2127
Jahoon Lee <aoleejohn@naver.com>
2022-07-20 03:27:58 +0900

    first commit

--- Cpp/Stage15/num11053.cpp
+++ Cpp/Stage15/num11053.cpp
@@ -1,19 +1,37 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace std;
 int main()
 {
-    int n,arr[1001],dp[1001]={0,},num_min=1001,cnt=0;
+    int n,cnt=1,num;
+    cin >> n;
+    vector<int> arr;
     for(int i=0;i<n;i++)
     {
-        cin >> arr[i];
-        if(arr[i]<num_min) num_min = arr[i];
+        cin >> num;
+        arr.push_back(num);
     }
-    for(int i=0;i<n;i++)
+    vector<int> arr_list(arr);
+    sort(arr_list.begin(),arr_list.end());
+    arr_list.erase(unique(arr_list.begin(),arr_list.end()),arr_list.end());
+    auto first = find(arr.begin(),arr.end(),arr_list[0]);
+    int a = first-arr.begin();
+    for(int i=1;i<arr_list.size();i++)
     {
-        
+        auto temp = find(arr.begin(),arr.end(),arr_list[i]);
+        int b= temp-arr.begin();
+        if(a > b)
+        {
+            arr.erase(temp);
+            i--;
+        }
+        else
+        {
+            first = temp;
+            a = first-arr.begin();
+        }
     }
-    
-
+    cout << cnt;
     return 0;
 }
