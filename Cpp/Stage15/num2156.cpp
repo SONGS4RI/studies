df73693b8ecf8c0f19d5776047bb15f2182a076c
Jahoon Lee <aoleejohn@naver.com>
2022-07-19 21:42:34 +0900

    first commit

--- Cpp/Stage15/num2156.cpp
+++ Cpp/Stage15/num2156.cpp
@@ -7,8 +7,7 @@ int main()
     for(int i=1;i<=n;i++) cin >> arr[i];
     dp[1] = arr[1];
     dp[2] = arr[1] + arr[2];
-    dp[3] = max(arr[1],arr[2]) + arr[3];
-    for(int i=4;i<=n;i++) dp[i] = max(dp[i-3]+arr[i-1]+arr[i],dp[i-1]);
+    for(int i=3;i<=n;i++) dp[i] = max(dp[i-1],max(arr[i]+dp[i-2],arr[i]+arr[i-1]+dp[i-3]));
     cout << dp[n];
     return 0;
 }
