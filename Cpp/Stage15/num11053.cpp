4322bf2803d7822cc3cad849140b9bd59cea31d5
Jahoon Lee <aoleejohn@naver.com>
2022-07-20 15:02:44 +0900

    first commit

--- Cpp/Stage15/num11053.cpp
+++ Cpp/Stage15/num11053.cpp
@@ -1,36 +1,35 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-int main()
-{
-    int n,num,res=0;
-    cin >> n;
-    vector<pair<int,int>> arr;
-    for(int i=0;i<n;i++)
-    {
-        cin >> num;
-        arr.push_back(pair<int,int>(num,i));
-    }
-    sort(arr.begin(),arr.end());
-    for(int i=1;i<n;i++)
-    {
-        if(arr[i].first==arr[i-1].first)
-        {
-            arr.erase(arr.begin()+i);
-            i--;
-            n--;
+
+int main() {
+    int T;
+    int N, M, K, H;
+    int X, Y;
+    int answer = 0;
+    
+    int dp[1001];
+    vector<int> v;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        cin >> K;
+        v.emplace_back(K);
+        int dp_max = 0;
+
+        for (int j = 0; j < v.size(); j++) {
+            if (v[i] > v[j]) {
+                if (dp_max < dp[j])
+                    dp_max = dp[j];
+            }
         }
+        dp[i] = dp_max + 1;
+        answer = max(answer, dp[i]);
     }
-    for(int i=0;i<arr.size();i++)
-    {
-        int cnt=1;
-        for(int j=0;j<i;j++)
-        {
-            if(arr[j].second<arr[i].second) cnt++;
-        }
-        if(cnt>res) res = cnt;
-    }
-    cout << res;
+
+    cout << answer << endl;
+
     return 0;
 }
