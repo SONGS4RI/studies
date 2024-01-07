288bdd95deeb73e5febee8f564e8fc1ac7ed59fe
이자훈 <jahlee@student.42seoul.kr>
2024-01-07 15:18:27 +0900

    :sparkles: feat: 스프링 예제 2 ing

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -8,12 +8,6 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        Member member = new Member(1L, "testMember", Grade.VIP);
-        memberService.join(member);
 
-        Member findMember = memberService.findMember(member.getId());
-        System.out.println("member = " + member.getName());
-        System.out.println("findMember = " + findMember.getName());
     }
 }
