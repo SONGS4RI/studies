b9fca0e54247c92f9bab06ef8b988a2da4fff23f
이자훈 <jahlee@student.42seoul.kr>
2024-01-09 16:19:46 +0900

    :sparkles: feat: 객체 지향 원리 적용 완료

---
+++ core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,16 @@
+package hello.core;
+
+import hello.core.member.MemberService;
+import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+
+    }
+}
