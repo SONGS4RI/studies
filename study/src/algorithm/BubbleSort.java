97905f47796f25bd07d84482adc27249e62f771e
이자훈 <jahlee@student.42seoul.kr>
2024-03-20 16:05:19 +0900

    :sparkles: feat: BubbleSort 수정

--- study/src/algorithm/BubbleSort.java
+++ study/src/algorithm/BubbleSort.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
 public class BubbleSort {
 	public static void main(String[] args) {
 		Integer[] list = {2, 6, 1, 5, 4, 3};
-		for (int i = 0; i < list.length - 1; i++) {
+		boolean sorted = false;
+		for (int i = 0; i < list.length - 1 && !sorted; i++) {
+			sorted = true;
 			for (int j = 1; j < list.length - i; j++) {
 				if (list[j - 1] > list[j]) {
+					sorted = false;
 					int tmp = list[j - 1];
 					list[j - 1] = list[j];
 					list[j] = tmp;
 				}
 			}
-			System.out.println(Arrays.toString(list));
+			System.out.println(i+1 + "회전: " + Arrays.toString(list));
 		}
 	}
 }
