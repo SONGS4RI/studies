9dad81d92fe0639dddd5a2e99720840883fe1ee5
이자훈 <jahlee@student.42seoul.kr>
2024-05-09 22:35:34 +0900

    :sparkles: feat: section3 ing

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import java.util.List;
+
 import jakarta.persistence.*;
 
 public class JpaMain {
@@ -9,8 +11,31 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);
 
-        em.close();
+            Member member1 = em.find(Member.class, 1L);
+            member1.setName("ByeA");
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            for (Member m : result) {
+                System.out.println(m);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
