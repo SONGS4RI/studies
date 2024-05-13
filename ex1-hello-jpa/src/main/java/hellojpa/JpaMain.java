e8fec9d7e0ade6db7569b5bd923ca260ec75f7da
이자훈 <jahlee@student.42seoul.kr>
2024-05-13 21:47:35 +0900

    :sparkles: feat: section3 ing

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,20 +14,12 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            // 비영속
-            Member member = new Member(150L, "A");
             // 영속
+            Member member = new Member(200L, "member200");
             em.persist(member);
 
-            Member member1 = em.find(Member.class, member.getId());
-            member1.setName("ByeA");
-
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .getResultList();
-
-            for (Member m : result) {
-                System.out.println(m);
-            }
+            em.flush();
+            System.out.println("-----------------");
 
             tx.commit();
             // 이때 모든 쿼리가 날아간다.
