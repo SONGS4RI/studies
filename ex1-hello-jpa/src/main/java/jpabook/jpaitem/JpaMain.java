a4b401fb0a688cc8b592b7a6be8a61e830982e1a
이자훈 <aoleejohn@gmail.com>
2024-07-03 18:10:00 +0900

    :sparkles: feat: section 10 ing

--- ex1-hello-jpa/src/main/java/jpabook/jpaitem/JpaMain.java
+++ ex1-hello-jpa/src/main/java/jpabook/jpaitem/JpaMain.java
@@ -4,9 +4,14 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import jpabook.jpaitem.domain.Member;
 import jpabook.jpaitem.domain.Order;
 import jpabook.jpaitem.domain.OrderItem;
+
+import java.util.List;
 
 public class JpaMain {
 	public static void main(String[] args) {
@@ -21,7 +26,18 @@ public class JpaMain {
 		   		" From Member m" +
 				" where m.name like '%kim%'", Member.class
 			);
+			CriteriaBuilder cb = em.getCriteriaBuilder();
+			CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
+			Root<Member> m = query.from(Member.class);
+			CriteriaQuery<Member> cq = query.select(m);
+			String name = "test";
+			if (name != null) {
+				cq = cq.where(cb.equal(m.get("name"), "kim"))
+			}
+
+			List<Member> resultList = em.createQuery(cq)
+					.getResultList();
 
 			tx.commit();
 		} catch (Exception e) {
