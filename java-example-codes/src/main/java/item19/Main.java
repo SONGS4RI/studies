ac151d5ab0541c8c6a9ad0ab09cf2226b80c7bb2
이자훈 <aoleejohn@gmail.com>
2024-08-22 17:22:37 +0900

    :sparkles: feat: item19

---
+++ java-example-codes/src/main/java/item19/Main.java
@@ -0,0 +1,12 @@
+package item19;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        LinkedList linkedList = new LinkedList();
+    }
+}
